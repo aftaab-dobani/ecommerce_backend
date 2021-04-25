ac580a757d93d41c1f3abf4c5744b6c0ea7d3c1a
Aftaab Dobani <1234dobani@gmail.com>
2021-04-25 12:56:49 -0400

    create new category and find category by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,14 +14,39 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+
   // find one category by its `id` value
   // be sure to include its associated Products
-});
+  router.get('/:id', async (req, res) => {
+    try {
+      const categoryData = await Category.findByPk(req.params.id, {
+        
+        include: [{ model: Product, as: 'category_product' }]
+      });
+  
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
-router.post('/', (req, res) => {
+
+
   // create a new category
-});
+  router.post('/', async (req, res) => {
+    try {
+      const categoryData = await Category.create(req.body);
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
